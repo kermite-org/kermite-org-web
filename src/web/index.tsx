c30b8c8d7e202eec3448cb798a151ed9cce43270
yahiro <yahiro07@users.noreply.github.com>
2021-10-02 07:42:50 +0900

    スタイルの適用を確認

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,7 +1,38 @@
-import { jsx, render } from '~/qx';
+import { applyGlobalStyle, css, jsx, render } from '~/qx';
+
+applyGlobalStyle(css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body,
+  #app {
+    height: 100%;
+  }
+
+  #app {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    overflow: hidden;
+    background: #fff;
+  }
+`);
+
+const rootStyle = css`
+  padding: 10px;
+  color: red;
+`;
 
 function start() {
-  render(() => <div>hello</div>, document.getElementById('app'));
+  render(
+    () => <div css={rootStyle}>hello</div>,
+    document.getElementById('app'),
+  );
 }
 
 window.addEventListener('load', start);
