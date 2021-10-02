91b15df1276861f5d64f751ad7d22b29fc3e6a61
yahiro <yahiro07@users.noreply.github.com>
2021-10-03 02:03:31 +0900

    IdeaSectionにアイコンを挿入

--- src/web/sections/IdeaSection.tsx
+++ src/web/sections/IdeaSection.tsx
@@ -1,28 +1,28 @@
 import { css, jsx } from 'qx';
-import { SectionBase } from '~/web/components/atoms';
+import { Icon, SectionBase } from '~/web/components/atoms';
 import { mqLarge, SectionFC } from '~/web/base';
 
 type IdeaItem = {
   title: string;
   text: string;
-  imageUrl: string;
+  iconSpec: string;
 };
 
 const ideaItems: IdeaItem[] = [
   {
     title: 'Beginner Friendly',
-    imageUrl: 'https://i.gyazo.com/00257ad39eff1b34e010ea461e4d72c1.png',
     text: 'ソフト単体でファームウェアの書き込みやキーマッピングの設定が簡単にできます。',
+    iconSpec: 'fa fa-child',
   },
   {
-    title: 'Batteries Included',
-    imageUrl: 'https://i.gyazo.com/00257ad39eff1b34e010ea461e4d72c1.png',
+    title: 'Ready to Use',
     text: '対応キーボードのファームウェアやキーマッピングのプリセットなどが同梱されており、すぐに使い始めることができます。',
+    iconSpec: 'fa fa-cube',
   },
   {
     title: 'Open Source',
-    imageUrl: 'https://i.gyazo.com/00257ad39eff1b34e010ea461e4d72c1.png',
     text: 'ファームウェアとユーティリティソフトのソースコードがMITライセンスで提供されています。',
+    iconSpec: 'fab fa-github-alt',
   },
 ];
 
@@ -32,7 +32,7 @@ export const IdeaSection: SectionFC = ({ altBgColor }) => (
       {ideaItems.map((item, idx) => (
         <div key={idx} class="item">
           <h3 class="text">{item.title}</h3>
-          <img class="image" src={item.imageUrl} />
+          <Icon spec={item.iconSpec} />
           <div>{item.text}</div>
         </div>
       ))}
@@ -56,12 +56,17 @@ const style = css`
       display: flex;
       flex-direction: column;
       align-items: center;
-      gap: 10px;
+      gap: 30px;
       font-size: 18px;
       line-height: 1.5em;
 
       > h3 {
         font-size: 28px;
+      }
+
+      > i {
+        color: #888;
+        font-size: 144px;
       }
     }
 
